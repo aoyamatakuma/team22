4c09f519d934c73e5d05914a48580472cc20653b
KPC-017A1400\ren <yama04210421@gmail.com>
2019-04-22 15:15:44 +0900

    anime-syon

---
+++ star4/Assets/Script/aoyama/script/CharacterController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    Rigidbody2D rigidPlayer;
+    public float speed = 3.0f;
+    public float jumpForce = 250.0f;
+    public bool isDead;
+    public bool isJump;
+    public bool isGround;
+    public bool isSimultaneous;
+    Animator anim;
+    float test = 0;
+    float count;
+    //public GameObject GameOverSE;
+    //public GameObject JumpSE;
+    // Use this for initialization
+
+    void Start()
+    {
+        rigidPlayer = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        isDead = false;
+        isJump = false;
+        isGround = false;
+        isSimultaneous = false;
+    }
+
+    void Update()
+    {
+        //count += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space)&&(isJump==true))
+        {
+            Jump();
+        }
+
+
+        float h = Input.GetAxis("Horizontal");
+
+        rigidPlayer.velocity = new Vector2(speed * h, rigidPlayer.velocity.y);
+        anim.SetFloat("run", Mathf.Abs(h));//runパラメーター
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            isSimultaneous = true;
+        }
+        else
+        {
+            isSimultaneous = false;
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.localScale.x > 0&& isSimultaneous == false|| Input.GetKey(KeyCode.RightArrow) && transform.localScale.x < 0&&isSimultaneous == false)
+        {
+            Vector2 pos = transform.localScale;
+            pos.x *= -1;
+            transform.localScale = pos;
+        }
+        
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            if (isDead) { return; }
+            isJump = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        anim.SetTrigger("stand");
+    }
+
+
+    void Jump()
+    {
+        anim.SetTrigger("jump");
+        rigidPlayer.velocity = Vector2.zero;
+        rigidPlayer.AddForce(Vector2.up * jumpForce);
+        isJump = false;
+    }
+}
