37482b8a59e519031da4decd2a633d29805ba66e
KPC-017A1400\ren <yama04210421@gmail.com>
2019-05-20 11:08:50 +0900

    キャラクターコントローラーのスクリプトを綺麗にしました

--- star4/Assets/Script/aoyama/script/CharacterController.cs
+++ star4/Assets/Script/aoyama/script/CharacterController.cs
@@ -6,53 +6,48 @@ public class CharacterController : MonoBehaviour
 {
     Rigidbody2D rigidPlayer;//物理演算
     Animator anim;//アニメーター
-
-    private float speed = 3.0f;//地上での移動速度
+    private float speed = 2.0f;//地上での移動速度
     private float airSpeed = 1.5f;//空中での移動速度
     private float jumpForce = 250.0f;//ジャンプの力
     public float stamina = 100;//体力
+    private float playerStamina;//体力代入
+    public float fallStamina=20;//体力減少
+    public float attackMove;//攻撃の移動範囲
+    public float damageMoveY;//ダメージを受けたときの移動範囲Y
+    public float damageMoveX;//ダメージを受けたときの移動範囲X
     private bool isJump;//ジャンプ判定
-    private bool isGround;//地面衝突判定
     public bool isAppeal;//アピール判定
     public bool isAttack;//攻撃状態判定
     public bool isOn;
     public bool islight;
-    public bool isBound;
-
+    public bool isBound;//頭の上でバウンド中か？
     string inputHorizontal;//コントローラースティック
     string inputJump;//コントローラーA
     string inputAppeal;//コントローラーX
     string inputAttack;//コントローラーB
-
     public bool isLeft;//左半身あたり判定
     public bool isRight;//右半身あたり判定
-
     public bool isLeftDir;//左向き
     public bool isRightDir;//右向き
 
 
     void Start()
     {
-        float playerStamina = 100;
-        rigidPlayer = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-
+        playerStamina = stamina;//スタミナ初期値
+        rigidPlayer = GetComponent<Rigidbody2D>();//物理演算取得
+        anim = GetComponent<Animator>();//アニメーション取得
         isJump = false;
-        isGround = false;
         isAppeal = false;
         isOn = false;
         isLeft = false;
         isRight = false;
-
         isLeftDir = false;
-        isRightDir = false; ;//右向き
-
-
-
-
+        isRightDir = false;
         isBound = false;
         isAttack = false;
+        islight = false;
 
+        //ここから先はゲームパッドの取得に使います。
         if (gameObject.tag == "Player1")
         {
             inputHorizontal = "GamePad1_Horizontal";
@@ -81,24 +76,59 @@ public class CharacterController : MonoBehaviour
             inputAppeal = "GamePad4_X";
             inputAttack = "GamePad4_B";
         }
-        islight = false;
+        //ここまで
     }
-
     void Update()
     {
         Jump();
+        Move();
+        Attack();        
+        
+        if (Input.GetButton(inputAppeal) && isJump == true)
+        {
+            isJump = false;
+            isAppeal = true;
+        }
+        if (Input.GetButtonUp(inputAppeal) && isAppeal == true)
+        {
+            isJump = true;
+            isAppeal = false;
+        }
 
+        if (isAppeal == true)
+        {
+
+            anim.SetTrigger("Appeal");
+            isOn = true;
+        }
+        if (isAppeal == false)
+        {
+            isOn = false;
+            anim.SetTrigger("Appealend");
+        }
+    }
+    void Jump()//ジャンプ系
+    {
+        if (Input.GetButton(inputJump) && (isJump == false) || isBound == true)//ジャンプボタンを押してなおかつジャンプ中じゃないとき
+        {
+            anim.SetTrigger("jump");
+            rigidPlayer.velocity = Vector2.zero;
+            rigidPlayer.AddForce(Vector2.up * jumpForce);
+            isJump = true;
+            isBound = false;
+        }
+    }
+    void Move()//移動系
+    {
         float h = Input.GetAxis(inputHorizontal);
-        float hh = Input.GetAxis("PlayerHorizontal");
-
-
+        #region//移動
         if (isAppeal != true)
         {
             if (isAttack == false)
             {
                 if (h != 0)
                 {
-                    if (isJump == true)//地上時のスピード
+                    if (isJump == false)//地上時のスピード
                         if (isLeft == true)//左に当たている時
                         {
                             if (h < 0)
@@ -129,7 +159,7 @@ public class CharacterController : MonoBehaviour
                     }
                 }
 
-                if (isJump == false)
+                if (isJump == true)
                 {
                     if (isLeft == true)//左に当たている時
                     {
@@ -153,15 +183,13 @@ public class CharacterController : MonoBehaviour
                             rigidPlayer.velocity = new Vector2(0, rigidPlayer.velocity.y);
                         }
                     }
-
                     if (isLeft == false && isRight == false)
                     {
 
                         rigidPlayer.velocity = new Vector2(airSpeed * h, rigidPlayer.velocity.y);
                     }
-
                 }
-                if (isJump == false)
+                if (isJump == true)
                 {
                     if (isLeft == true)//左に当たている時
                     {
@@ -185,25 +213,15 @@ public class CharacterController : MonoBehaviour
                             rigidPlayer.velocity = new Vector2(0, rigidPlayer.velocity.y);
                         }
                     }
-
                     if (isLeft == false && isRight == false)
                     {
 
                         rigidPlayer.velocity = new Vector2(airSpeed * h, rigidPlayer.velocity.y);
                     }
-
                 }
             }
-
-            if (hh != 0)
-            {
-
-                rigidPlayer.velocity = new Vector2(speed * hh, rigidPlayer.velocity.y);
-
-            }
-            anim.SetFloat("run", Mathf.Abs(h));//runパラメーター         
-
-
+            #endregion
+            #region//向き判定
             if (Input.GetAxis(inputHorizontal) > 0f && transform.localScale.x < 0 || Input.GetAxis(inputHorizontal) < 0f && transform.localScale.x > 0)
             {
                 Vector2 pos = transform.localScale;
@@ -212,7 +230,6 @@ public class CharacterController : MonoBehaviour
                 {
                     isLeftDir = false;
                     isRightDir = true;
-                    Debug.Log("→");
                     transform.GetChild(0).gameObject.transform.localScale = new Vector3(1, 1, 1);
                     transform.GetChild(1).gameObject.transform.localScale = new Vector3(1, 1, 1);
                 }
@@ -220,108 +237,41 @@ public class CharacterController : MonoBehaviour
                 {
                     isLeftDir = true;
                     isRightDir = false;
-                    Debug.Log("←");
                     transform.GetChild(0).gameObject.transform.localScale = new Vector3(-1, 1, 1);
                     transform.GetChild(1).gameObject.transform.localScale = new Vector3(-1, 1, 1);
                 }
                 transform.localScale = pos;
             }
+            #endregion
         }
-
-
-        if (Input.GetButton(inputAppeal) && isJump == true)
-        {
-            isJump = false;
-            isAppeal = true;
-
-        }
-        if (Input.GetButtonUp(inputAppeal) && isAppeal == true)
-        {
-            isJump = true;
-            isAppeal = false;
-        }
-
-        if (isAppeal == true)
-        {
-
-            anim.SetTrigger("Appeal");
-            isOn = true;
-        }
-        if (isAppeal == false)
-        {
-            isOn = false;
-            anim.SetTrigger("Appealend");
-        }
-        StartCoroutine("Attack");//コルーチンスタート
+        anim.SetFloat("run", Mathf.Abs(h));//runパラメーター         
     }
-
-
-    void OnTriggerStay2D(Collider2D col)
+    void Attack()
     {
-        if (col.gameObject.tag == "Ground")
+        if(playerStamina<100)
         {
-            isJump = true;
+            playerStamina += 0.1f;
         }
-
-        if (col.gameObject.tag == "LeftBody")
+        if (playerStamina >= fallStamina&&Input.GetButtonDown(inputAttack) && isAttack == false&&isJump==false&&isBound==false)
         {
-            isLeft = true;
-        }
-        if (col.gameObject.tag == "RightBody")
-        {
-            isRight = true;
-        }
-
-    }
-    void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.gameObject.tag == "LeftBody")
-        {
-            isLeft = false;
-        }
-        if (col.gameObject.tag == "RightBody")
-        {
-            isRight = false;
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        anim.SetTrigger("stand");
-        if (col.gameObject.tag == "head")
-        {
-            isBound = true;
-        }
-
-        if (col.gameObject.tag == "RightAttack")
-        {
-            rigidPlayer.velocity = new Vector2(3, 5);
-        }
-        if (col.gameObject.tag == "LeftAttack")
-        {
-            rigidPlayer.velocity = new Vector2(-3, 5);
-        }
-    }
-
-
-    void Jump()
-    {
-        if (Input.GetButton(inputJump) && (isJump == true) || isBound == true)
-        {
-            anim.SetTrigger("jump");
-            rigidPlayer.velocity = Vector2.zero;
-            rigidPlayer.AddForce(Vector2.up * jumpForce);
-            isJump = false;
-            isBound = false;
-        }
-    }
-
-    public IEnumerator Attack()
-    {
-        if (Input.GetButtonDown(inputAttack) && isAttack == false && isJump == true)
-        {
-            transform.GetChild(1).gameObject.SetActive(true);
             isAttack = true;
+            StartCoroutine("AttackStart");//コルーチンスタート
+        }
+        Debug.Log(playerStamina);
+    }
+    public IEnumerator AttackStart()//アタック（コルーチン使用）
+    {
+            transform.GetChild(1).gameObject.SetActive(true);//攻撃オブジェクトを出す
+            #region//スタミナ減少
+            if (playerStamina < fallStamina)
+            {
+                playerStamina = 0;
+            }
+            else
+            {
+                playerStamina = playerStamina - fallStamina;
+            }
+            #endregion
             if (transform.localScale.x > 0)
             {
                 rigidPlayer.velocity = new Vector2(7, rigidPlayer.velocity.y);
@@ -333,6 +283,47 @@ public class CharacterController : MonoBehaviour
             yield return new WaitForSeconds(0.3f);
             transform.GetChild(1).gameObject.SetActive(false);
             isAttack = false;
+    }
+    void OnTriggerStay2D(Collider2D col)//常に当たっている時
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isJump = false;
+        }
+        if (col.gameObject.tag == "LeftBody")
+        {
+            isLeft = true;
+        }
+        if (col.gameObject.tag == "RightBody")
+        {
+            isRight = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)//離れたか時
+    {
+        if (col.gameObject.tag == "LeftBody")
+        {
+            isLeft = false;
+        }
+        if (col.gameObject.tag == "RightBody")
+        {
+            isRight = false;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)//当たった時
+    {
+        anim.SetTrigger("stand");
+        if (col.gameObject.tag == "head")
+        {
+            isBound = true;
+        }
+        if (col.gameObject.tag == "RightAttack")
+        {
+            rigidPlayer.velocity = new Vector2(3, 5);
+        }
+        if (col.gameObject.tag == "LeftAttack")
+        {
+            rigidPlayer.velocity = new Vector2(-3, 5);
         }
     }
 }
