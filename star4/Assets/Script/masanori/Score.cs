8fc7a508cec66e3ce91f90d432a606c1b32ae77d
masa <uverworld.exile49@gmail.com>
2019-05-31 14:38:49 +0900

    リザルトでスコアがすべて出そろったらタイトルへ行けるように変更

--- star4/Assets/Script/masanori/Score.cs
+++ star4/Assets/Script/masanori/Score.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
     public static List<int> scores=new List<int>();
 
     public List<int> obj = new List<int>();
+
+
+    public static int scoreEndCount;
+    bool isEndScore;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreEndCount = 0;
+        isEndScore = false;
     }
 
     // Update is called once per frame
@@ -18,10 +24,34 @@ public class Score : MonoBehaviour
     {
 
         obj = scores;
+
+        ScoreEnd();
     }
 
     public static void AddScore(int score)
     {
         scores.Add(score);
+    }
+
+    void ScoreEnd()
+    {
+
+        Debug.Log(scoreEndCount);
+        if (scoreEndCount==4)
+        {
+            
+            isEndScore = true;
+            scoreEndCount = 0;
+        }
+
+        if(isEndScore && Input.GetButton("GamePad1_Jump"))
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+
+    public static void EndCount()
+    {
+        scoreEndCount+=1;
     }
 }
