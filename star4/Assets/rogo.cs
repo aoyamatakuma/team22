b17b993f964df65b63ecc93d24e6f64a83869b86
KPC-017A1400\ren <yama04210421@gmail.com>
2020-02-06 11:22:31 +0900

    UNITY追加

---
+++ star4/Assets/rogo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class rogo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("End");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator End()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene("Title");
+    }
+}
